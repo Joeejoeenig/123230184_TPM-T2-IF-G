bb3bbbe939f14d33214c04cf5fafc91c9daf9743
Joeejoeenig <joeejoeenig@gmail.com>
2026-07-18 04:08:15 +0700

    commit
    commit

--- SmartExpense/lib/theme/app_theme.dart
+++ SmartExpense/lib/theme/app_theme.dart
@@ -1,3 +1,36 @@
+// import 'package:flutter/material.dart';
+
+// class AppTheme {
+//   static ThemeData get lightTheme {
+//     return ThemeData(
+//       useMaterial3: true,
+//       colorSchemeSeed: Colors.green,
+//       scaffoldBackgroundColor: const Color(0xFFF5F7FA),
+
+//       appBarTheme: const AppBarTheme(
+//         centerTitle: true,
+//         backgroundColor: Colors.green,
+//         foregroundColor: Colors.white,
+//         elevation: 0,
+//       ),
+
+//       cardTheme: CardTheme(
+//         elevation: 3,
+//         color: Colors.white,
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(16),
+//         ),
+//       ),
+
+//       inputDecorationTheme: InputDecorationTheme(
+//         border: OutlineInputBorder(
+//           borderRadius: BorderRadius.circular(12),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class AppTheme {
@@ -14,9 +47,9 @@ class AppTheme {
         elevation: 0,
       ),
 
-      cardTheme: CardTheme(
-        elevation: 3,
+      cardTheme: CardThemeData(
         color: Colors.white,
+        elevation: 3,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
         ),
